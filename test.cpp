eaaa754d350924a81babede7cf29d1db6e623018
Kurkuzabra <84335717+Kurkuzabra@users.noreply.github.com>
2024-02-16 16:31:09 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,38 +1,11 @@
 #include "main.h"
-#include <cassert>
-
-#define assertm(exp, msg) assert(((void)msg, exp))
-
-using namespace std;
-using namespace lft;
-
-int main()
-{
-    assertm(2 + 2 == 4, "TEST0 failed");
-    cout << "Test 0 passed" << endl;
-
-    assertm(LongFloat(0) == LongFloat(0.0), "TEST1 failed");
-    cout << "Test 1 passed" << endl;    
-    assertm(LongFloat(234.546) - LongFloat(234.546) == LongFloat(0), "TEST2 failed");
-    cout << "Test 2 passed" << endl;
-    assertm(LongFloat(234.546) - LongFloat(-234.546) == LongFloat(469.092), "TEST3 failed");
-    cout << "Test 3 passed" << endl;
-    assertm(LongFloat("234.546") == LongFloat(234.546), "TEST4 failed");
-    cout << "Test 4 passed" << endl;
-    assertm(LongFloat("234.546") * LongFloat(0.785) == LongFloat("184.11861"), "TEST5 failed");
-    cout << "Test 5 passed" << endl;
-    assertm(longDivision(LongFloat("234.546"), LongFloat("-0.785"), 4) == LongFloat("-298.7847"), "TEST6 failed");
-    cout << "Test 6 passed" << endl;
-    assertm(LongFloat("0") == LongFloat("-0"), "TEST7 failed");
-    cout << "Test 7 passed" << endl;
-    assertm(LongFloat("-1") < LongFloat("1"), "TEST8 failed");
-    cout << "Test 8 passed" << endl;
-    assertm(LongFloat("0") <= LongFloat("-0"), "TEST9 failed");
-    cout << "Test 9 passed" << endl;
-    assertm(LongFloat("1") >= LongFloat("0.5"), "TEST10 failed");
-    cout << "Test 10 passed" << endl;
-    assertm(LongFloat("1") > LongFloat("-2"), "TEST11 failed");
-    cout << "Test 11 passed" << endl;
-    assertm(LongFloat("-1") != LongFloat("1"), "TEST12 failed");
-    cout << "Test 12 passed" << endl;
+#include <gtest/gtest.h>
+ 
+TEST(SquareRootTest, PositiveNos) { 
+    ASSERT_EQ(lft::LongFloat(0), lft::LongFloat(0.0));
+}
+ 
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
