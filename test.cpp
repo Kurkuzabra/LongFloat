3a4efa8c1466b8e66fc4338ddfb089b6e3d24170
Kurkuzabra <84335717+Kurkuzabra@users.noreply.github.com>
2024-02-16 17:18:50 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,19 +1,12 @@
 #include <gtest/gtest.h>
 
-using namespace std;
-
-TEST(TestGroupName, Subtest_1) {
-  ASSERT_TRUE(1 == 1);
-}
-
-TEST(TestGroupName, Subtest_2) {
-  ASSERT_FALSE('b' == 'b');
-  cout << "continue test after failure" << endl;
-}
-
-int main(int argc, char **argv)
+TEST(module_name, test_name)
 {
-  ::testing::InitGoogleTest(&argc, argv);
+        EXPECT_FALSE(false);
+}
 
-  return RUN_ALL_TESTS();
+int main(int argc, char** argv)
+{
+        testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
 }
