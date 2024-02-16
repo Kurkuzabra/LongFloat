faae0812f0d09b76ebabd0027c9e5b12adb6669c
Kurkuzabra <84335717+Kurkuzabra@users.noreply.github.com>
2024-02-16 16:36:50 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,11 +1,21 @@
 #include "main.h"
 #include <gtest/gtest.h>
- 
-TEST(SquareRootTest, PositiveNos) { 
-    ASSERT_EQ(lft::LongFloat(0), lft::LongFloat(0.0));
+
+using namespace std;
+using namespace lft;
+
+TEST(TestGroupName, Subtest_1) {
+  ASSERT_TRUE(1 == 1);
 }
- 
-int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+
+TEST(TestGroupName, Subtest_2) {
+  ASSERT_FALSE('b' == 'b');
+  cout << "continue test after failure" << endl;
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
 }
