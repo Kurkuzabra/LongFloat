188656872e3401d7b7ee2d952c7400179688c41d
Kurkuzabra <84335717+Kurkuzabra@users.noreply.github.com>
2024-02-16 17:23:23 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,8 +1,23 @@
 #include <gtest/gtest.h>
+#include <main.h>
+
+using namespace std;
+using namespace lft;
 
 TEST(module_name, test_name)
 {
-        EXPECT_FALSE(false);
+        EXPECT_TRUE(2 + 2 == 4);
+        EXPECT_TRUE(LongFloat(0) == LongFloat(0.0));
+        EXPECT_TRUE(LongFloat(234.546) - LongFloat(234.546) == LongFloat(0));
+        EXPECT_TRUE(LongFloat(234.546) - LongFloat(-234.546) == LongFloat(469.092));
+        EXPECT_TRUE(LongFloat("234.546") == LongFloat(234.546));
+        EXPECT_TRUE(LongFloat("234.546") * LongFloat(0.785) == LongFloat("184.11861"));
+        EXPECT_TRUE(longDivision(LongFloat("234.546"), LongFloat("-0.785"), 4) == LongFloat("-298.7847"));
+        EXPECT_TRUE(LongFloat("0") == LongFloat("-0"));
+        EXPECT_TRUE(LongFloat("-1") < LongFloat("1"));
+        EXPECT_TRUE(LongFloat("0") <= LongFloat("-0"));
+        EXPECT_TRUE(LongFloat("1") >= LongFloat("0.5"));
+        EXPECT_TRUE(LongFloat("1") > LongFloat("-2"));
 }
 
 int main(int argc, char** argv)
